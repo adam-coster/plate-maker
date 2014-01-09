27447197210bd83cc9c47efde1dd9ae773b1d186
Adam Coster <software@adamcoster.com>
2014-01-09 11:09:20 -0600

    Functional version complete. In testing.

--- plateMaker.py
+++ plateMaker.py
@@ -68,12 +68,59 @@ def getLimits( superset, question ):
             start = superset.index(ends[0])
             end   = superset.index(ends[1])
             limits += superset[start:end+1]
+    # Sort them to deal with middle-zeros (if present)
+    limits = [ l.zfill(2) for l in limits ]
+    limits = sorted(limits)
+    limits = [re.sub(r'0([\d\w])$',r'\1',l) for l in limits]
     return limits
 
-def expandWellInput( rows, cols, annotateWells ):
+def expandWellInput( rows, cols, message ):
+    allWells = [r+c for r in rows for c in cols]
+    rawInput = input( message ).strip().upper()
+    if rawInput == 'ALL': return allWells
+    if rawInput == '':    return []
     
+    wellSets = rawInput.split(' ')
     
-    
+    # Allowed input formats are row, row-row, col, col-col,
+    # rowcol, rowcol-rowcol
+    # special keywords: all
+    rowColRegex = r'^([A-Z]{0,1})(\d{0,2})-{0,1}([A-Z]{0,1})(\d{0,2})$'
+    wellSubset = []
+    for wellSet in wellSets:
+        if wellSet == 'ALL': return allWells
+        
+        # Break into row and column parts
+        try:
+            rowStart, colStart, rowEnd, colEnd = re.match(rowColRegex,wellSet).groups()
+        except:
+            input('Something was wrong with your input.')
+            exit()
+        
+
+        # Fill out the missing values
+        if rowEnd   == '':
+            if rowStart=='':
+                rowStart = rows[0]
+                rowEnd   = rows[-1] 
+            else:
+                rowEnd = rowStart
+
+        if colEnd   == '':
+            if colStart=='':
+                colStart = cols[0]
+                colEnd   = cols[-1] 
+            else:
+                colEnd = colStart
+        
+        subRows = [chr(r) for r in range(ord(rowStart),ord(rowEnd)+1)]
+        subCols = [str(c) for c in range(int(colStart),int(colEnd)+1)]
+        
+        subWells = [r+c for r in subRows for c in subCols if r in rows and c in cols ]
+        wellSubset += subWells
+        
+    return wellSubset
+            
 def main():
     
     # Choose plate type
@@ -94,6 +141,7 @@ def main():
     # Allow for annotating limited subsets
     while True:
         experimentName = input( 'Experiment name: ' )
+        
         if experimentName.upper() in ['','QUIT','Q','EXIT']: break
         
         setLimits = yesNoBoolean( 'Limit to a subset of wells? ', False )
@@ -107,18 +155,37 @@ def main():
         annotations.update({w:{'experiment':experimentName} for w in allWells})
         
         print( experimentName + ' occurs only in the following wells:')
-        displayPlate(plateRows,plateCols,allWells)
         
         # Now that we have boundaries,
         # allow for more complex selections
         # within. For each selection, add
         # annotations.
         while True:
-            annotateWells = input('Wells to annotate: ').strip()
-            if len(annotateWells) == 0: break
+            displayPlate(plateRows,plateCols,allWells)
+            wells = expandWellInput( rows, cols, 'Wells to annotate: ' )
+            if len(wells) == 0: break
             
-            wells = expandWellInput( rows, cols, annotateWells )
-        
+            print('You have selected the following wells:')
+
+            while True:
+                displayPlate( plateRows, plateCols, wells )
+                field = input('Field: ').strip()
+                if field.upper() in ['QUIT','EXIT','Q','']: break
+                annotationFields += field
+                
+                value = input('Value: ').strip()
+                for well in wells:
+                    annotations[well][field] = value
+                    
+        # Should now have complete set of annotations
+    savename = input('File savename: ').strip() + '.csv'
+    save = open(savename,'w')
+    save.write('well,' + ','.join(annotationFields) + '\n' )
+    for well in annotations.keys():
+        save.write( well + ',' )
+        values = [annotations[well].get(field,'') for field in annotationFields]
+        save.write( ','.join(values) + '\n' )
+    save.close()
     
 if __name__ == '__main__':
     main()
